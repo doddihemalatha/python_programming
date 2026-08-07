df490048ea093879eadf9cd5ea4b1b12446062ff
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-07 22:51:52 +0530

    Enhance addition program with user input and comments
    Updated the program description and added logic comments.

---
+++ 01_Python_Basics/01_Addition.py
@@ -0,0 +1,20 @@
+"""
+Program: Addition of two Numbers
+Author: Hema
+Concepts Used: Variables, Input, Arithmetic Operator (+), Output
+Description: Accepts two numbers from the user, performs addition, and displays the result.
+"""
+
+#LOGIC 
+# Accept two numbers.
+# Add them.
+# Store the result.
+# Display the result.
+
+
+#CODE
+num1 = int(input("Enter first Number:"))
+num2  = int(input("Enter second Number:"))
+
+sums = num1+num2
+print("Addition:",sums)
