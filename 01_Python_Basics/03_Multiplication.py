88af1377499260f89815ab3e51548313d71d9db0
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-07 23:35:13 +0530

    Create 03_Multiplication.py

---
+++ 01_Python_Basics/03_Multiplication.py
@@ -0,0 +1,19 @@
+"""
+Program :Multiplication of Two Numbers
+Author  : Hemalatha
+concept used : varaibles, input, arthimetic operator(*), type casting
+Description : Multiply the Two numbers and display the result
+"""
+
+#LOGIC
+# Accept input
+# Multiply the two numbers
+# store in result
+# display result
+
+#CODE  
+num1 = int(input("Enter First Number:"))
+num2 = int(input("Enter Second Number:"))
+
+result = num1 * num2
+print("Multiplication:",result)
