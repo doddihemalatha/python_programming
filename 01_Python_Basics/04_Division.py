8fcc98b15467242da53dd2ffc0a033b10a4e9e96
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-07 23:37:16 +0530

    Add Division program for two numbers
    This script takes two numbers as input and displays their division result.

---
+++ 01_Python_Basics/04_Division.py
@@ -0,0 +1,19 @@
+"""
+Program : Division of Two Numbers
+Author  : Hemalatha
+concept used : varaibles, input, arthimetic operator(/), type casting
+Description : Divide the two numbers and disply the result
+"""
+
+#LOGIC
+# Accept input
+# Divide the two numbers
+# store in result
+# display result
+
+#CODE  
+num1 = int(input("Enter First Number:"))
+num2 = int(input("Enter Second Number:"))
+
+result = num1 / num2
+print("Division:",result)
