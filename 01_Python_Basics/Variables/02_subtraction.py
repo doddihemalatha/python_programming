9de8f133b42693c3de411cedfa21ee1c9a79b54b
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-07 23:05:47 +0530

    Create 02_subtraction.py

---
+++ 01_Python_Basics/Variables/02_subtraction.py
@@ -0,0 +1,19 @@
+"""
+Program : Addition of Two Numbers
+Author  : Hemalatha
+concept used : varaibles, input, arthimetic operator(-), type casting
+Description : Subtracts one number from another.
+"""
+
+#LOGIC
+#Accept input
+#subtract from one number from another
+#store in result
+#display result
+
+#CODE  
+num1 = int(input("Enter First Number:"))
+num2 = int(input("Enter Second Number:"))
+
+result = num1+num2
+print("Subtraction:",result)
