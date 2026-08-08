a8eb2840a5d3801e4bb3aa6e7b6855641f94fbfa
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-08 19:25:12 +0530

    Create 07_Cube.py

---
+++ 01_Python_Basics/07_Cube.py
@@ -0,0 +1,26 @@
+"""
+Program : Cube of a Number
+Author  : Hemalatha
+
+Concepts Used:
+- Variables
+- Input
+- Type Casting
+- Arithmetic Operator (**)
+- Output
+
+Description:
+Accepts a number from the user and calculates its cube.
+"""
+
+#LOGIC
+# Accept a number from the user
+# Calculate its cube using the ** operator
+# Store the result
+# Display the result
+
+#CODE
+num = int(input("Enter a Number:"))
+
+result = num ** 3
+print("Cube:",result)
