af2b265eee3db56e3511782898776f87a7fcf53d
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-08 19:01:30 +0530

    Create 06_Square.py

---
+++ 01_Python_Basics/06_Square.py
@@ -0,0 +1,27 @@
+"""
+Program : Square of a Number
+Author  : Hema
+
+Concepts Used:
+- Variables
+- Input
+- Type Casting
+- Arithmetic Operator (**)
+- Output
+
+Description:
+Accepts a number from the user and calculates its square.
+"""
+
+#LOGIC
+# Accept a Number from the user
+# Calculate its square using the ** operator
+# Store the result
+# Display the result
+
+#CODE
+num1 = int(input("Enter first Number:"))
+num2 = int(input("Enter second Number:"))
+
+result = num1 ** num2
+print("Square is:",result)
