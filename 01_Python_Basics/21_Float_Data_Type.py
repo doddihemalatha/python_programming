d4e4336d7123f9bbdcb348c90a5a2e2bfa86a90d
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-10 22:04:48 +0530

    Create 21_Float_Data_Type.py

---
+++ 01_Python_Basics/21_Float_Data_Type.py
@@ -0,0 +1,24 @@
+"""
+Program : Float Data Type
+Author  : Hema
+
+Concepts Used:
+- Float
+- Variables
+- type()
+- Output
+
+Description:
+Demonstrates how to store and identify a decimal value in Python.
+"""
+
+# Logic
+# Store a decimal number in a variable.
+# Check its data type using type().
+# Display the value and its data type.
+
+#code
+num = 25.5
+
+print("Value =", num)
+print("Data Type =", type(num))
