983274bf4956f3945b86554497f58c2717d4fc3b
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-10 22:06:37 +0530

    Create 22_String_Data_Type.py

---
+++ 01_Python_Basics/22_String_Data_Type.py
@@ -0,0 +1,23 @@
+"""
+Program : String Data Type
+Author  : Hemalatha
+
+Concepts Used:
+- String
+- Variables
+- type()
+- Output
+
+Description:
+Demonstrates how to store and identify text data in Python.
+"""
+
+# Logic
+# Store text in a variable.
+# Check its data type using type().
+# Display the value and its data type.
+
+name = "Hema"
+
+print("Value =", name)
+print("Data Type =", type(name))
