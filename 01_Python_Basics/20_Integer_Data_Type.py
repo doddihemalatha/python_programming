81ec808f3a8bd19907095f8984a87d21bb5c909f
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-10 22:03:14 +0530

    Create 20_Integer_Data_Type.py

---
+++ 01_Python_Basics/20_Integer_Data_Type.py
@@ -0,0 +1,24 @@
+"""
+Program : Integer Data Type
+Author  : Hema
+
+Concepts Used:
+- Integer
+- Variables
+- type()
+- Output
+
+Description:
+Demonstrates how to store and identify an integer value in Python.
+"""
+
+# Logic
+# Store a whole number in a variable.
+# Check its data type using type().
+# Display the value and its data type.
+
+#code
+num = 19
+
+print("value = ",num)
+print("Data Type = ",type(num))
