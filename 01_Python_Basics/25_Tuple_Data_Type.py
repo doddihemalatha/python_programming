afcd9804bcbaaafd8bc83f295e77dd7bef0294ea
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-10 22:22:15 +0530

    Add example for tuple data type in Python
    Demonstrates tuple usage and type identification in Python.

---
+++ 01_Python_Basics/25_Tuple_Data_Type.py
@@ -0,0 +1,25 @@
+"""
+Program : Tuple Data Type
+Author  : Hemalatha
+
+Concepts Used:
+- Tuple
+- Variables
+- type()
+- Output
+
+Description:
+Demonstrates how to store multiple values in a tuple
+and identify its data type.
+"""
+
+# Logic
+# Store multiple values in a tuple.
+# Check its data type using type().
+# Display the tuple and its data type.
+
+#code
+data = (10, "hema", 25.5, True)
+
+print("Tuple =", data)
+print("Data Type =", type(data))
