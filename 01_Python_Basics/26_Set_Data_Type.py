7247f82cd610ace95261b9f26cc85904de98ffb5
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-10 22:30:02 +0530

    Add program to demonstrate Python set data type
    This program illustrates how to create a set in Python, store unique values, and check the data type using the type() function.

---
+++ 01_Python_Basics/26_Set_Data_Type.py
@@ -0,0 +1,24 @@
+"""
+Program : Set Data Type
+Author  : Hemalatha
+
+Concepts Used:
+- Set
+- Variables
+- type()
+- Output
+
+Description:
+Demonstrates how to store multiple unique values in a set
+and identify its data type.
+"""
+
+# Logic
+# Store multiple values in a set.
+# Check its data type using type().
+# Display the set and its data type.
+
+#code
+data = {10,20,30,40}
+print("Set = ",data)
+print("Data Type = ",type(data))
