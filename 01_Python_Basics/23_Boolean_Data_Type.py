e1f751b6e4306b0378de7fd4e5fd5fce8c71f864
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-10 22:10:14 +0530

    Create 23_Boolean_Data_Type.py

---
+++ 01_Python_Basics/23_Boolean_Data_Type.py
@@ -0,0 +1,24 @@
+"""
+Program : Boolean Data Type
+Author  : Hema
+
+Concepts Used:
+- Boolean
+- Variables
+- type()
+- Output
+
+Description:
+Demonstrates how to store and identify Boolean values in Python.
+"""
+
+# Logic
+# Store a Boolean value in a variable.
+# Check its data type using type().
+# Display the value and its data type.
+
+#code
+is_student = True
+
+print("Value =", is_student)
+print("Data Type =", type(is_student))
