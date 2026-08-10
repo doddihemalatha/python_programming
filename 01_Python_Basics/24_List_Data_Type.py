3386a98406c5a1a6970c36b98f0b7d638adf6ac7
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-10 22:18:54 +0530

    Create 24_List_Data_Type.py

---
+++ 01_Python_Basics/24_List_Data_Type.py
@@ -0,0 +1,25 @@
+"""
+Program : List Data Type
+Author  : Hema
+
+Concepts Used:
+- List
+- Variables
+- type()
+- Output
+
+Description:
+Demonstrates how to store multiple values in a list
+and identify its data type.
+"""
+
+# Logic
+# Store multiple values in a list.
+# Check its data type using type().
+# Display the list and its data type.
+
+#code
+data = [10,"hema",25.5,True]
+
+print("List = ",data)
+print("Data Type = ",type(data))
