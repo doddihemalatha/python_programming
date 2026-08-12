e2eb9442b17331ee2d4c34f035f7d29b825200dd
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-12 20:06:01 +0530

    Create 32_Str_Type_Casting.py

---
+++ 01_Python_Basics/32_Str_Type_Casting.py
@@ -0,0 +1,27 @@
+"""
+Program : String Type Casting
+Author  : Hemalatha
+
+Concepts Used:
+- Type Casting
+- str()
+- Variables
+- type()
+- Output
+
+Description:
+Converts a number into a string.
+"""
+
+# Logic
+# Store a number.
+# Convert it into a string using str().
+# Display the converted value and its data type.
+
+#code
+num = 25
+
+converted_num = str(num)
+
+print("value = ",converted_num)
+print("Datatype : ",type(converted_num))
