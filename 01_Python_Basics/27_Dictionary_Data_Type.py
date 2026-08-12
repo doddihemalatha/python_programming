e9da8543ecd41dbe2638c13b5e9669691118bc99
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-12 18:56:54 +0530

    Add dictionary data type demonstration script
    This script demonstrates how to store data as key-value pairs using a dictionary and check its data type.

---
+++ 01_Python_Basics/27_Dictionary_Data_Type.py
@@ -0,0 +1,30 @@
+"""
+Program : Dictionary Data Type
+Author  : Hemalatha
+
+Concepts Used:
+- Dictionary
+- Variables
+- Key-Value Pairs
+- type()
+- Output
+
+Description:
+Demonstrates how to store data as key-value pairs
+and identify its data type.
+"""
+
+# Logic
+# Store data using key-value pairs.
+# Check its data type using type().
+# Display the dictionary and its data type.
+
+#code
+student  = {
+"name" : "Hemalatha",
+"age"  : 19,
+"course" : "Data Science"
+}
+
+print("Dictionary : ",student)
+print("Data Type : ",type(student))
