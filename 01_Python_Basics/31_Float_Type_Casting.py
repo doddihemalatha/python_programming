88b22c9c6da8f2521fb56ce4b3ee1bbdc793d7c4
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-12 19:56:29 +0530

    Create 31_Float_Type_Casting.py

---
+++ 01_Python_Basics/31_Float_Type_Casting.py
@@ -0,0 +1,27 @@
+"""
+Program : Float Type Casting
+Author  : Hema
+
+Concepts Used:
+- Type Casting
+- float()
+- Variables
+- type()
+- Output
+
+Description:
+Converts an integer value into a floating-point number.
+"""
+
+# Logic
+# Store an integer value.
+# Convert it into a float using float().
+# Display the converted value and its data type.
+
+#code
+num = 12
+
+converted_num = float(num)
+
+print("Value : ",converted_num)
+print("Datatype : ",type(converted_num))
