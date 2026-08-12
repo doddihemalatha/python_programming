699e316b2d4595e3f15a95d337017e89b68e6f25
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-12 19:12:41 +0530

    Create 28_Range_Data_Type.py

---
+++ 01_Python_Basics/28_Range_Data_Type.py
@@ -0,0 +1,25 @@
+"""
+Program : Range Data Type
+Author  : Hema
+
+Concepts Used:
+- Range
+- Variables
+- type()
+- Output
+
+Description:
+Demonstrates how to create a range of numbers
+and identify its data type.
+"""
+
+# Logic
+# Create a range of numbers.
+# Check its data type using type().
+# Display the range and its data type.
+
+#code
+num = range(1,6)
+
+print("Range : ",num)
+print("Data Type : ",type(num))
