551f1df83c7b1030ed6abb2d1bf17968639c784b
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-12 19:50:35 +0530

    Create 30_Int_Type_Casting.py

---
+++ 01_Python_Basics/30_Int_Type_Casting.py
@@ -0,0 +1,26 @@
+"""
+Program : Integer Type Casting
+Author  : Hema
+
+Concepts Used:
+- Type Casting
+- int()
+- Variables
+- type()
+- Output
+
+Description:
+Converts a string value into an integer.
+"""
+
+# Logic
+# Store a number as a string.
+# Convert the string into an integer using int().
+# Display the converted value and its data type.
+
+#code
+num = "25"
+
+converted_num = int(num)
+print("value : ",converted_num)
+print("Data Type : ",type(converted_num))
