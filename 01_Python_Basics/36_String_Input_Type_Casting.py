ff96f71f4e09e73969de3c18a57d260922e11099
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-14 23:00:42 +0530

    Create 36_String_Input_Type_Casting.py

---
+++ 01_Python_Basics/36_String_Input_Type_Casting.py
@@ -0,0 +1,24 @@
+"""
+Program : String Input Type Casting
+Author  : Hemalatha
+
+Concepts Used:
+- input()
+- Type Casting
+- str()
+- Variables
+- Output
+
+Description:
+Accepts a value from the user and explicitly converts it
+to a string using str().
+"""
+
+# Logic
+# Accept a value from the user.
+# Display the value and its data type.
+
+#code
+value = input("Enter a Value: ")
+print("Value : ",value)
+print("Data Type =", type(value))
