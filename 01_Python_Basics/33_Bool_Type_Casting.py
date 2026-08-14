73662ed161d443428ddf029a398b34dcbb3e69c2
doddihemalatha <198776300+doddihemalatha@users.noreply.github.com>
2026-08-14 22:42:50 +0530

    Add Boolean Type Casting program in Python
    This program demonstrates Boolean type casting in Python by converting various values to Boolean using the bool() function and displaying the results.

---
+++ 01_Python_Basics/33_Bool_Type_Casting.py
@@ -0,0 +1,26 @@
+"""
+Program : Boolean Type Casting
+Author  : Hemalatha
+
+Concepts Used:
+- Type Casting
+- bool()
+- Variables
+- type()
+- Output
+
+Description:
+Converts different values into Boolean values using bool().
+"""
+
+# Logic
+# Store a value.
+# Convert the value into a Boolean using bool().
+# Display the converted value and its data type.
+
+#code
+value = ""
+converted_value = bool(value)
+
+print("Converted Value : ",converted_value)
+print("Data Type : ",type(converted_value)
